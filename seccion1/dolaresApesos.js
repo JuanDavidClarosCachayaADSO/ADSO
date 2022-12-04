8be00d868a104a411a2ce1d6de0dd2c6392b0948
JuanDavidClarosCachayaADSO <juroscachaya@gmail.com>
2022-12-04 14:32:39 -0500

    adso

---
+++ seccion1/dolaresApesos.js
@@ -0,0 +1,6 @@
+
+let cantidadDolar=parseInt(prompt("ingrese la cantidad de dolares "));
+let peso = 4.764;
+let conversion=(cantidadDolar*peso);
+
+alert(`${cantidadDolar} dolares equivalen a ${conversion} pesos colombianos `);
---
+++ seccion1/temperatura.js
@@ -0,0 +1,5 @@
+let centigrados = 9;
+let fahrenheit = ((centigrados*1.8)+32);
+
+alert(`los 9 centigrados de temperatuta en new yourk equivalen a ${fahrenheit} fahrenheit`);
+
