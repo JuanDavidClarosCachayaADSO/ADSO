30325342c641b8a85bf927a523f949d50f1753f3
JuanDavidClarosCachayaADSO <juroscachaya@gmail.com>
2022-12-04 14:32:47 -0500

    adso

---
+++ clase22oct/ejercicio1.js
@@ -0,0 +1,2 @@
+let resultado = 1+2+3+4+5+6+7+8+9+10;
+alert(`la sumatoria de los numeros entre 1 y 10 es: ${resultado}`);
---
+++ clase22oct/ejercicio2.js
@@ -0,0 +1,8 @@
+
+let catetoAd=parseInt(prompt("ingrese el valor del cateto adyacente"));
+let catetoOp=parseInt(prompt("ingrese el valor del cateto opuesto"));
+let operacion=Math.sqrt((catetoAd*catetoAd)+(catetoOp*catetoOp));
+
+alert(`la hipotenusa es igual a:${operacion} `);
+//alert(`la hipotenusa es : ${Math.sqrt(catetoAd*catetoAd)+(catetoOp*catetoOp)}`);
+
