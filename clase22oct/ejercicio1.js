1bcbbecbebb345e7a636d505d152ff61263bb840
JuanDavidClarosCachayaADSO <112283085+JuanDavidClarosCachayaADSO@users.noreply.github.com>
2022-12-04 14:37:48 -0500

    Delete clase22oct directory

--- clase22oct/ejercicio1.js
+++
@@ -1,2 +0,0 @@
-let resultado = 1+2+3+4+5+6+7+8+9+10;
-alert(`la sumatoria de los numeros entre 1 y 10 es: ${resultado}`);
--- clase22oct/ejercicio2.js
+++
@@ -1,8 +0,0 @@
-
-let catetoAd=parseInt(prompt("ingrese el valor del cateto adyacente"));
-let catetoOp=parseInt(prompt("ingrese el valor del cateto opuesto"));
-let operacion=Math.sqrt((catetoAd*catetoAd)+(catetoOp*catetoOp));
-
-alert(`la hipotenusa es igual a:${operacion} `);
-//alert(`la hipotenusa es : ${Math.sqrt(catetoAd*catetoAd)+(catetoOp*catetoOp)}`);
-
